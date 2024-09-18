f7459304ff8f9a67722026f5316bb7396c5a4891
greyflame321 <greyson.m.winchester@gmail.com>
2024-09-18 17:48:56 -0400

    added platforms to L1 (#6)

--- src/scenes/level1.ts
+++ src/scenes/level1.ts
@@ -2,16 +2,21 @@ import { Scene } from "phaser";
 
 export class Level1 extends Scene {
   preload() {
-    this.load.image("sky", "public/sky.png");
-    this.load.image("platform", "public/platform.png");
-    this.load.image("bomb", "public/bomb.png");
-    this.load.image("star", "public/star.png");
+    this.load.image("sky", "/sky.png");
+    this.load.image("ground", "/platform.png");
+    this.load.image("bomb", "/bomb.png");
+    this.load.image("star", "/star.png");
   }
 
   create() {
     this.add.image(400, 300, "sky");
-    this.add.image(400, 400, "star");
-    this.add.image(400, 600, "platform").setScale(2, 1);
-    this.add.image(400, 580, "bomb");
+
+    const platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    platforms.create(600, 400, "ground");
+    platforms.create(50, 250, "ground");
+    platforms.create(750, 220, "ground");
   }
 }
